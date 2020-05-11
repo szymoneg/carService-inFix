fa6dbaac96319ddfe18a529a44c80bb7bb8f568b
Szymon Bilinski <szymon.zsp5@gmail.com>
2020-05-12 00:07:12 +0200

    +database connection +database select, insert

--- src/main/java/com/company/infix/InfixApplication.java
+++ src/main/java/com/company/infix/InfixApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InfixApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(InfixApplication.class, args);
     }
-
 }
---
+++ src/main/java/com/company/infix/dao/SpringJdbcConfig.java
@@ -0,0 +1,23 @@
+package com.company.infix.dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.company.infix")
+public class SpringJdbcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/infix");
+        dataSource.setUsername("root");
+        dataSource.setPassword("zaq1@WSX");
+
+        return dataSource;
+    }
+}
